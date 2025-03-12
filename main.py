6108b26a659caeccc365a7c74a0e0e1a56c4a6d1
jtufano <jtufano90@gmail.com>
2025-03-12 17:15:17 -0400

    Added a window for the game space.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import pygame, constants
+import pygame
+from constants import *
 
 def main():
+    pygame.init()
+
     print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
